3fa08ccb16edffb292fc9ff6015603b11548f0a9
diego <di.goncalvesr@gmail.com>
2024-05-23 01:44:19 -0300

    Conteúdo das aulas de Operadores e variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,17 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        //estudar classe String que representa texto na aplicação
+
+        int idade; //Tipo "int", nome "idade", com nenhum valor atribuído. 
+        int anoFabricacao = 2021; //tipo "int", nome "anoFabricacao", com valor 2021.
+        double salarioMinimo = 2.500; //tipo "double", nome "salarioMinimo", valor 2.500.
+
+        // TiposEVariaveis.java
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = numeroNormal;
+
     }
 }
